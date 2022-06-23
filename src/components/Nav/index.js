87263383640d47b49439b8574726c2b6325b7ba4
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-06-22 23:42:55 -0700

    added tests for nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -23,18 +23,14 @@ function Nav() {
   return (
     <header data-testid="header" className="flex-row px-1">
       <h2>
-        <a href="/">
-          <span role="img" aria-label="camera">
-            {" "}
-            📸
-          </span>{" "}
-          Oh Snap!
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about" onClick={() => categorySelected()}>
+            <a data-testid="about" href="#about">
               About me
             </a>
           </li>
