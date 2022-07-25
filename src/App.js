204d38fc1c5fe8735eddb8703bc544a887e19210
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-07-25 11:15:26 -0700

    added photolist component

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
       ></Nav>
       <main>
         <div>
-          <Gallery></Gallery>
+          <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
         </div>
       </main>
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import { capitalizeFirstLetter } from "../../utils/helpers";
-import photo from '../../assets/small/commercial/0.jpg';
+import PhotoList from '../PhotoList';
 
-function Gallery(props) {
-  const currentCategory = {
-    name: "commercial",
-    description: "Photos of grocery stores, food trucks, and other commercial projects",
-  };  
+function Gallery({ currentCategory }) {
+  const { name, description } = currentCategory
   return (
     <section>
-      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-      <p>{currentCategory.description}</p>
-      <div>
-        <img src={photo} />
-      </div>
+      <h1 data-testid='h1tag'>{capitalizeFirstLetter(name)}</h1>
+      <p>{description}</p>
+      <PhotoList category={currentCategory.name} />
     </section>
   );
 }
