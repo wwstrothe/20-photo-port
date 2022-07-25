9f89d20e504a8e7ae9ff43cf67642c28f3c5850d
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-07-25 11:46:25 -0700

    updated tests for all suites and all tests pass

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,21 +1,19 @@
+// __tests__/About.test.js
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
-import '@testing-library/jest-dom/extend-expect';
-import About from '..';
+import "@testing-library/jest-dom/extend-expect";
+import About from "..";
 
-afterEach(cleanup)
+afterEach(cleanup);
 
-describe('About component', () => {
-  // First Test
-  it(`renders`, () => {
+describe("About component renders", () => {
+  it("renders", () => {
     render(<About />);
   });
 
-  // Second Test
-  it('matches snapshot DOM node structure', () => {
-    // render About
+  it("matches snapshot DOM node structure", () => {
     const { asFragment } = render(<About />);
-    expect(asFragment()).toMatchSnapshot();
-  })
 
-})
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,3 +1,4 @@
+// __tests__/Nav.test.js with hard coded categories
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
@@ -5,32 +6,30 @@ import Nav from "..";
 
 afterEach(cleanup);
 
-describe('Nav component', () => {
-  // baseline test
+describe("Nav component", () => {
   it("renders", () => {
     render(<Nav />);
   });
 
-  // snapshot test
-  it('matches snapshot', () => {
-    //render Nav
+  it("matches snapshot", () => {
     const { asFragment } = render(<Nav />);
-    expect(asFragment()).toMatchSnapshot();
-  })
-})
 
-describe('emoji is visible', () => {
-  it('inserts emoji into the h2', () => {
-    // Arrange
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe("emoji is visible", () => {
+  it("inserts emoji into the h2", () => {
     const { getByLabelText } = render(<Nav />);
-    // Assert
+
     expect(getByLabelText("camera")).toHaveTextContent("📸");
-  })
-})
+  });
+});
 
 describe("links are visible", () => {
   it("inserts text into the links", () => {
     const { getByTestId } = render(<Nav />);
+
     expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
     expect(getByTestId("about")).toHaveTextContent("About me");
   });
