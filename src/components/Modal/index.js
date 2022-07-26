d3fe2fe189491a8d57396f8be4ff03431a1ac3db
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-07-25 23:14:54 -0700

    modal close

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Modal({currentPhoto}) {
+function Modal({ onClose, currentPhoto }) {
   const {name, category, description, index} = currentPhoto;
 
   return (
@@ -9,7 +9,9 @@ function Modal({currentPhoto}) {
         <h3 className="modalTitle">{name}</h3>
         <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
         <p>{description}</p>
-        <button type="button">
+        <button 
+          onClick={onClose}
+          type="button">
           Close this modal
         </button>
       </div>
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -106,15 +106,16 @@ const PhotoList = ({ category }) => {
   const [currentPhoto, setCurrentPhoto] = useState();
 
   const toggleModal = (image, i) => {
-    // current photo
     setCurrentPhoto({...image, index: i})
-    setIsModalOpen(true)
+    setIsModalOpen(!isModalOpen)
   }
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto}/>}
+      {isModalOpen && (
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
